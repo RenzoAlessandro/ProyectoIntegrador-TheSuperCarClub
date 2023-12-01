65483527dd24af89bd3cbe9e340cf9f50b891392
Renzo Alessandro <renzo.sucari@ucsp.edu.pe>
2023-12-01 11:17:20 -0500

    Actualizacion

--- js/printsCards.js
+++ js/printsCards.js
@@ -29,13 +29,13 @@ printTestimonialCard(testimonialsArray, RecentTestimonialsHTML);
 function hexToRGB(hex){
     //  convertimos hex a rgb
   // 3 digits
-  if (hex.length == 4) {
+  if (hex.length() == 4) {
     r = "0x" + hex[1] + hex[1];
     g = "0x" + hex[2] + hex[2];
     b = "0x" + hex[3] + hex[3];
 
   // 6 digits
-  } else if (hex.length == 7) {
+  } else if (hex.length() == 7) {
     r = "0x" + hex[1] + hex[2];
     g = "0x" + hex[3] + hex[4];
     b = "0x" + hex[5] + hex[6];
