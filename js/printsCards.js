37f747c112d3fc53f3d2ee1134f9c5070a9a4784
Renzo Alessandro <renzo.sucari@ucsp.edu.pe>
2023-12-01 11:24:04 -0500

    .

--- js/printsCards.js
+++ js/printsCards.js
@@ -29,13 +29,13 @@ printTestimonialCard(testimonialsArray, RecentTestimonialsHTML);
 function hexToRGB(hex){
     //  convertimos hex a rgb
   // 3 digits
-  if (hex.length == 4) {
+  if (hex.length === 4) {
     r = "0x" + hex[1] + hex[1];
     g = "0x" + hex[2] + hex[2];
     b = "0x" + hex[3] + hex[3];
 
   // 6 digits
-  } else if (hex.length == 7) {
+  } else if (hex.length === 7) {
     r = "0x" + hex[1] + hex[2];
     g = "0x" + hex[3] + hex[4];
     b = "0x" + hex[5] + hex[6];
@@ -76,6 +76,7 @@ function hexToRGB(hex){
 }
 
 function getPastelHigthColor(hex){
+  
   hue = hexToRGB(hex);
   // Calculate lightness
   l = 96;
@@ -101,6 +102,7 @@ function goProductDetail(id){
 function printFirstCard(listModelCars, containerHTML){
   containerHTML.innerHTML = '';
   listModelCars.forEach(car => {
+    console.log(car.color),
     containerHTML.innerHTML += `
                                 <a onclick=goProductDetail(${car.id})>
                                   <article class="card-cars-first">
